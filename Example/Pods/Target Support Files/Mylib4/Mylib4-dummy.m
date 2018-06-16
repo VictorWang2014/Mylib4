c7d0aca05f217ab93de1ba2dcb6cf40e9541b955
VictorWang2014 <admin@192.168.0.105>
2018-06-16 12:32:06 +0800

    update

---
+++ Example/Pods/Target Support Files/Mylib4/Mylib4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Mylib4 : NSObject
+@end
+@implementation PodsDummy_Mylib4
+@end
---
+++ Example/Pods/Target Support Files/Pods-Mylib4_Example/Pods-Mylib4_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mylib4_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Mylib4_Example
+@end
